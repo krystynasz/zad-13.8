6d9fab11141427f837c0ca6bdd5cab6f52440909
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-07-03 20:18:53 +0200

    Add project files

---
+++ index.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+
+server.on('request', function (request, response) {
+   
+    if (request.method === 'GET' && request.url === '/victory') {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
+        fs.readFile('./victory.html', 'utf-8', function (err, data) {
+            response.write(data);
+            response.end();
+        });
+
+    } else {
+        /*fs.readFile('./error.html', 'utf-8', function (err, data) {
+            response.statusCode = 404;
+            response.write(data);
+            response.end();
+        });*/
+        fs.readFile('image.jpg', function (err, data) {
+            response.statusCode = 404;
+            response.setHeader("Content-Type", 'image/jpeg');
+            response.end(data);
+    
+        });
+    };
+});
+
+server.listen(7000);
